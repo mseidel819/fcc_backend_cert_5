4fe270ef1dd6b54fca6a921821eb31a8d0564a0b
Matt Seidel <mseidel819@gmail.com>
2023-04-05 16:03:53 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config({ path: "./.env" });
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
+  const data = req.file;
+  const { originalname, mimetype, size } = req.file;
+  console.log(data);
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
